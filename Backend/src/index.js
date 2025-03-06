89cd0c96e9effceb26c60e986a18d54ddf3101f6
Deepanshu8945 <Deepanshu010101@gmail.com>
2025-03-06 16:25:59 +0530

    PROJECT COMPLETED(deployment process)

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -4,6 +4,8 @@ import messageRoutes from "./routes/message.route.js"
 import dotenv from "dotenv"
 import cors from "cors"
 
+import path from "path"
+
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser"
 import {app,server} from "./lib/socket.js"
@@ -11,6 +13,7 @@ import {app,server} from "./lib/socket.js"
 dotenv.config();
 
 const PORT = process.env.PORT
+const __dirname = path.resolve();
 
 app.use(cors({
   origin:"http://localhost:5173",
@@ -21,6 +24,15 @@ app.use(cookieParser())
 app.use(express.json())
 app.use("/api/auth" , authRoutes)
 app.use("/api/messages" , messageRoutes)
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+
+  app.get("*", (req,res)=>{
+    res.sendFile(path.join(__dirname, "../frontend", "dist" , "index.js"))
+  })
+}
 
 
 server.listen(PORT, () => {
